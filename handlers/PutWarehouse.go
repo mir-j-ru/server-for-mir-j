02a102b5f8c57b74d23bdfcc082dc31a294e41ea
mir-j-ru <vania200005k@gmail.com>
2026-09-07 15:24:40 +0300

    Update PutWarehouse.go

--- handlers/PutWarehouse.go
+++ handlers/PutWarehouse.go
@@ -35,7 +35,7 @@ func PutWarehoyse(w http.ResponseWriter, r *http.Request, dbConn *sql.DB){
         return
     }
 
-    err = db.PutWarehoyse(dbConn, number, category)
+    err = db.PutWarehoyse(dbConn, id, category)
     if err!=nil{
         http.Error(w, "error db:", http.StatusInternalServerError)
         return
